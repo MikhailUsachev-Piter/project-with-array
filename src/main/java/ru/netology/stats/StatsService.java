a95c35f0433d19d30d9da381c2bc73a18750618c
MikhailUsachev-Piter <mikhail.usachev@gmail.com>
2020-05-20 12:17:52 +0300

    Added files

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,4 +1,57 @@
 package ru.netology.stats;
-
 public class StatsService {
+
+    public int calculateSum(int[] purchases) {
+        int sumSales = 0;
+        for (int index : purchases) {
+            sumSales += index; }
+        return sumSales;
+    }
+
+    public int calculateAverageAmount(int[] purchases) {
+        int averageSum = calculateSum(purchases) / purchases.length;
+        return averageSum;
+    }
+
+    public int salesHigherMiddle(int[] purchases) {
+        int averageSum = calculateAverageAmount(purchases);
+        int quantityHight = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            if (averageSum < purchases[i])
+                quantityHight++; }
+        return quantityHight;
+    }
+
+    public int salesLowMiddle(int[] purchases) {
+        int averageSum = calculateAverageAmount(purchases);
+        int quantitylow = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            if (averageSum > purchases[i])
+                quantitylow++; }
+        return quantitylow;
+    }
+
+    public int calculateMin(int[] purchases) {
+        int minSales = purchases[0];
+        int month = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            int monthSale = purchases[i];
+            if (minSales >= monthSale) {
+                minSales = monthSale;
+                month = i + 1; }
+        }
+        return month;
+    }
+
+    public int calculateMax(int[] purchases) {
+        int maxSales = purchases[0];
+        int month = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            int monthSales = purchases[i];
+            if (maxSales <= monthSales) {
+                maxSales = monthSales;
+                month = i + 1; }
+        }
+        return month;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,5 +1,55 @@
-import static org.junit.jupiter.api.Assertions.*;
+package ru.netology.stats;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatsServiceTest {
+    int[] purchases = {8, 15, 13, 15, 20, 8, 19, 20, 16, 14, 14, 18};
 
+    @Test
+    void sumSales() {
+        StatsService sumSales = new StatsService();
+        int sum = sumSales.calculateSum(purchases);
+        assertEquals(180, sum);
+//        System.out.println("Сумму всех продаж: " + sum);
+    }
+
+    @Test
+    void averageAmount() {
+        StatsService averageAmount = new StatsService();
+        int averageSum = averageAmount.calculateAverageAmount(purchases);
+        assertEquals(15, averageSum);
+//        System.out.println("Среднюю сумму продаж в месяц: " + averageSum);
+    }
+
+    @Test
+    void salesHigherMiddle() {
+        StatsService quantityHight = new StatsService();
+        int value = quantityHight.salesHigherMiddle(purchases);
+        assertEquals(5, value);
+//        System.out.println("Кол-во месяцев, в которых продажи были выше среднего: " + value);
+    }
+
+    @Test
+    void salesLowMiddle() {
+        StatsService quantityLow = new StatsService();
+        int value = quantityLow.salesLowMiddle(purchases);
+        assertEquals(5, value);
+//        System.out.println("Кол-во месяцев, в которых продажи были ниже среднего: " + value);
+    }
+
+    @Test
+    void monthMinSales() {
+        StatsService minSales = new StatsService();
+        int month = minSales.calculateMin(purchases);
+        assertEquals(6, month);
+//        System.out.println("Номер месяца, в котором был минимум продаж: " + month);
+    }
+
+    @Test
+    void monthMaxSales() {
+        StatsService maxSales = new StatsService();
+        int month = maxSales.calculateMax(purchases);
+        assertEquals(8, month);
+//        System.out.println("Номер месяца, в котором был пик продаж: " + month);
+    }
 }
