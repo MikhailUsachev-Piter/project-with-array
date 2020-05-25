dc0ffca6cf3bd311c7f139a3dd9b9190caf2f99a
MikhailUsachev-Piter <mikhail.usachev@gmail.com>
2020-05-25 13:38:01 +0300

    Added

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology.stats;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatsServiceTest {
@@ -10,7 +12,6 @@ class StatsServiceTest {
         StatsService sumSales = new StatsService();
         int sum = sumSales.calculateSum(purchases);
         assertEquals(180, sum);
-//        System.out.println("Сумму всех продаж: " + sum);
     }
 
     @Test
@@ -18,7 +19,6 @@ class StatsServiceTest {
         StatsService averageAmount = new StatsService();
         int averageSum = averageAmount.calculateAverageAmount(purchases);
         assertEquals(15, averageSum);
-//        System.out.println("Среднюю сумму продаж в месяц: " + averageSum);
     }
 
     @Test
@@ -26,7 +26,6 @@ class StatsServiceTest {
         StatsService quantityHight = new StatsService();
         int value = quantityHight.salesHigherMiddle(purchases);
         assertEquals(5, value);
-//        System.out.println("Кол-во месяцев, в которых продажи были выше среднего: " + value);
     }
 
     @Test
@@ -34,7 +33,6 @@ class StatsServiceTest {
         StatsService quantityLow = new StatsService();
         int value = quantityLow.salesLowMiddle(purchases);
         assertEquals(5, value);
-//        System.out.println("Кол-во месяцев, в которых продажи были ниже среднего: " + value);
     }
 
     @Test
@@ -42,7 +40,6 @@ class StatsServiceTest {
         StatsService minSales = new StatsService();
         int month = minSales.calculateMin(purchases);
         assertEquals(6, month);
-//        System.out.println("Номер месяца, в котором был минимум продаж: " + month);
     }
 
     @Test
@@ -50,6 +47,5 @@ class StatsServiceTest {
         StatsService maxSales = new StatsService();
         int month = maxSales.calculateMax(purchases);
         assertEquals(8, month);
-//        System.out.println("Номер месяца, в котором был пик продаж: " + month);
     }
 }
